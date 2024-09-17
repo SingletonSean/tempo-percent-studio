f6dc598a648b45cf1dcdfc924346b80d87454ae9
SingletonSean <singletonsean4@gmail.com>
2024-09-16 20:44:59 -0400

    Use event to command behavior to load personal bests

--- TempoPercentStudio.MAUI/Pages/CalculateTempoView.xaml.cs
+++ TempoPercentStudio.MAUI/Pages/CalculateTempoView.xaml.cs
@@ -1,29 +1,11 @@
-using System.Windows.Input;
-
 namespace TempoPercentStudio.MAUI.Pages;
 
 public partial class CalculateTempoView : ContentPage
 {
-    public static readonly BindableProperty OnAppearingCommandProperty =
-    BindableProperty.Create(nameof(OnAppearingCommand), typeof(ICommand), typeof(CalculateTempoView), null);
-
-    public ICommand OnAppearingCommand
-    {
-        get => (ICommand)GetValue(OnAppearingCommandProperty);
-        set => SetValue(OnAppearingCommandProperty, value);
-    }
-
     public CalculateTempoView(CalculateTempoViewModel viewModel)
 	{
 		InitializeComponent();
 
 		BindingContext = viewModel;
 	}
-
-    protected override void OnAppearing()
-    {
-        OnAppearingCommand?.Execute(null);
-
-        base.OnAppearing();
-    }
 }
--- TempoPercentStudio.MAUI/Pages/PersonalBestListingView.xaml.cs
+++ TempoPercentStudio.MAUI/Pages/PersonalBestListingView.xaml.cs
@@ -1,29 +1,11 @@
-using System.Windows.Input;
-
 namespace TempoPercentStudio.MAUI.Pages;
 
 public partial class PersonalBestListingView : ContentPage
 {
-	public static readonly BindableProperty OnAppearingCommandProperty =
-		BindableProperty.Create(nameof(OnAppearingCommand), typeof(ICommand), typeof(PersonalBestListingView), null);
-
-	public ICommand OnAppearingCommand
-	{
-		get => (ICommand)GetValue(OnAppearingCommandProperty);
-		set => SetValue(OnAppearingCommandProperty, value);
-	}
-
 	public PersonalBestListingView(PersonalBestListingViewModel viewModel)
 	{
 		InitializeComponent();
 
 		BindingContext = viewModel;
 	}
-
-    protected override void OnAppearing()
-    {
-		OnAppearingCommand?.Execute(null);
-
-        base.OnAppearing();
-    }
 }
